7fcbdbfc5e0fc2bfc8b0090a0f58a14c83c1fe44
Ana-Maria Voicila <ana-maria.voicila.15@ucl.ac.uk>
2017-09-03 12:57:46 +0200

    nuti e desteapta

--- app/components/Browser/index.js
+++ app/components/Browser/index.js
@@ -4,6 +4,11 @@ import './style.scss'
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://192.168.0.29:8000');
 
+var Shake = require('shake.js');
+var myShakeEvent = new Shake({
+  threshold: 15
+});
+
 class Browser extends React.Component {
 
   constructor() {
@@ -14,7 +19,8 @@ class Browser extends React.Component {
       isMobile: this.isMobile(),
       renderBrowser: false,
       toRender: undefined,
-      animated: false
+      animated: false,
+      hasTwoFingers: false
     }
 
     socket.on('testing', data => {
@@ -33,12 +39,33 @@ class Browser extends React.Component {
     if (this.isMobile()) {
       this.setState({ shouldRender: true });
     }
+
+    // window.addEventListener('scroll', this.handleScroll.bind(this));
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+    window.addEventListener('touchstart', this.hasTwoFingers.bind(this));
+
+    // setTimeout(() => {
+    //   this.refs.browser.addEventListener('mousedown', (e) => {
+    //     alert("mousedown");
+    //   })
+    // }, 1500)
+
+    // myShakeEvent.start()
+  }
+
+  componentWillUnmount() {
+    // window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  shakeEventDidOccur() {
+    alert('shake!');
   }
 
   isMobile() {
     return window.innerWidth < 640;
   };
 
+  
   test() {
     socket.emit('testing', this.refs.browser.outerHTML);
   }
@@ -47,25 +74,46 @@ class Browser extends React.Component {
     this.setState({ animated: true });
   }
 
+  hasTwoFingers(e) {
+    if (e.touches.length == 2) {
+      this.setState({ hasTwoFingers: true });
+    }
+  }
+
+  handleScroll(e) {
+    if (this.state.hasTwoFingers) {
+      this.refs.anutica.style.display = "none"
+      this.setState({ hasTwoFingers: false })
+    }
+
+    console.log(document.getElementsByTagName('body')[0].scrollTop)
+
+    if (document.getElementsByTagName('body')[0].scrollTop > 60) {
+      this.test()
+    }
+  }
+
   render() {
     return (
       <div className="browser">
-        <button className="button" onClick={this.test.bind(this)}>Test</button>
-        <button className="button" onClick={this.anutica.bind(this)}>Anuta</button>
+        {/* <button className="button" onClick={this.test.bind(this)}>Test</button>
+        <button className="button" onClick={this.anutica.bind(this)}>Anuta</button> */}
+
+        <div ref="anutica" className="anutica" onTouchStart={this.hasTwoFingers.bind(this)}></div>
 
         {/* <div className="fade-container">
           <p className={this.state.animated ? "fade-enter-active" : "fade-enter"}>Anutica e animata</p>
         </div> */}
 
-        {this.state.renderBrowser && !this.state.isMobile ?
-          <div className="fade-container browser-frame-container" dangerouslySetInnerHTML={{ __html: this.state.toRender }} />
-          :
-          <div></div>
-        }
+          {this.state.renderBrowser && !this.state.isMobile ?
+            <div className="browser-frame-container" dangerouslySetInnerHTML={{ __html: this.state.toRender }} />
+            :
+            <div></div>
+          }
 
         {this.state.shouldRender ?
-          <div className="browser-frame-container">
-            <object ref="browser" className={this.state.animated ? "fade-enter-active browser-frame" : "fade-enter browser-frame"} type="text/html" data="https://razvangeangu.com"></object>
+          <div ref="browserContainer" className="browser-frame-container">
+            <object ref="browser" className="browser-frame" type="text/html" data="https://razvangeangu.com"></object>
           </div>
           :
           ''
